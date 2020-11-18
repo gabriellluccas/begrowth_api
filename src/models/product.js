c3b091c8eaf3b3a05052fe96513527658e2dd269
Gabriell Lucas <gabriellluccas@gmail.com>
2020-11-18 19:48:02 -0300

    :sparkles: Adicionando modelo do produto no mongoose
    Adicionando o model do produto no mongoose usando no id o identificador unico universal

---
+++ src/models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const uuid = require('uuid');
+
+const ProductSchema = new Schema({
+  _id: {
+    type: String,
+    default: uuid.v4,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    description: 'product name',
+  },
+  price: {
+    type: Number,
+    required: true,
+    description: 'product price',
+  },
+  image_url: {
+    type: String,
+    required: true,
+    description: 'url for product image',
+  },
+  created: {type: Date, default: Date.now},
+  updated: {type: Date, default: Date.now},
+}, {_id: false});
+
+module.exports = mongoose.model('products', ProductSchema);
