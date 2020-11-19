d58f1a5fbe04cff7cc42ae1db24436758e15387d
Gabriell Lucas <gabriellluccas@gmail.com>
2020-11-18 21:45:43 -0300

    :sparkles: Criando o resto dos serviços do produto
    Criando os serviços: buscar produtos, buscar produto por id, editar produto por id, deletar produto por id

--- src/services/product.js
+++ src/services/product.js
@@ -3,6 +3,18 @@ const {product: Product} = require('../models');
 const product = {
   create: ({name, price, imageUrl}) =>
     Product.create({name, price, image_url: imageUrl}),
+
+  findAll: () => Product.find({}),
+
+  findById: (id) => Product.findById(id),
+
+  findByIdAndUpdate: (id, obj = {}) => {
+    const options = {new: true, useFindAndModify: false};
+    const objToUpdate = {...obj, updated: Date.now()};
+    return Product.findByIdAndUpdate(id, objToUpdate, options);
+  },
+
+  findByIdAndDelete: (id) => Product.findByIdAndDelete(id),
 };
 
 module.exports = product;
