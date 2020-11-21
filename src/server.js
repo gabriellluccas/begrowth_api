b2fc3e434a69b8e5a3f653bfd97f98bf6ca2a19d
Gabriell Lucas <gabriellluccas@gmail.com>
2020-11-20 23:09:07 -0300

    :bug: Aplicando cors
    Aplicando cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const serverFunction = async (dbConnection) => {
   const server = express();
   server.use(express.json());
+
+  const {APP_URL} = process.env;
+  corsOptions = {
+    origin: APP_URL || 'http://localhost:3000',
+  };
+  server.use(cors());
 
   await dbConnection.open();
   server.use(router);
